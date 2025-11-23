024cf45cb1ebd72fe06f8a84e78ed183102e3037
luo chongpeng <haringkarolt@gmail.com>
2025-11-23 23:27:41 +0800

    洛谷3156题

---
+++ luogu/luogu_ P3156.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int m, n, temp;
+    cin >> n >> m;
+    vector<int> student;
+    for(int i = 0; i < n; i++){
+        cin >> temp;
+        student.push_back(temp);
+    }
+    for(int i = 0; i < m; i++){
+        cin >> temp;
+        cout << student[temp - 1] << "\n";
+        //endl换行比较慢，如果比赛超时可以换\n
+    }
+
+    return 0;
+}
