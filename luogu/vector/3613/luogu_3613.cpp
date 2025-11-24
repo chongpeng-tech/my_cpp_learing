2f7855f1d9b8b8b7e0efd2abc9bfa1d544b24dcd
luo chongpeng <haringkarolt@gmail.com>
2025-11-24 20:24:36 +0800

    还没做完，先提交先

---
+++ luogu/vector/3613/luogu_3613.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,q,temp,num,tempNums,tempItem;
+    cin >> n >> q;
+    struct box{
+        int nums; //编号
+        int item; //物品
+    };
+    vector<box> Box(n + 1);
+    for(int i = 0; i < q; i++){
+        cin >> temp;
+        if(temp == 1){
+            cin >> num >> tempNums >> tempItem;
+            Box
+        }
+
+    }
+
+
+
+    return 0;
+}
